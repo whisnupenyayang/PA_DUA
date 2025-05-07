3f74e7ef832486bd783edf4725df254b71c22eb1
whisnupenyayang <whisnu.abraham.l.s@gmail.com>
2025-05-07 21:13:05 +0700

    controller dan model sementara toko dan resep

---
+++ markopiApp/backend/Back-End-Reengineering-Markopi/app/Models/Resep.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Resep extends Model
+{
+    use HasFactory;
+
+    protected $table = 'Resep';
+
+    protected $fillable = [
+        'nama_resep',
+        'deskripsi',
+        'gambar_resep',
+    ];
+}
+
---
+++ markopiApp/backend/Back-End-Reengineering-Markopi/app/Models/Toko.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Toko extends Model
+{
+    use HasFactory;
+
+    protected $table = 'Toko';
+
+    protected $fillable = [
+        'nama_toko',
+        'lokasi',
+        'jam_operasional',
+        'foto_toko',
+    ];
+}
+
