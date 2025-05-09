03c68787742bae2035d9399c78db7dae812aac73
elisabethpasaribu <elipasaribu0@gmail.com>
2025-05-09 11:45:46 +0700

    update tampilan beranda

--- markopiApp/markopi/lib/view/Beranda/MainMenu.dart
+++ markopiApp/markopi/lib/view/Beranda/MainMenu.dart
@@ -11,12 +11,12 @@ class MainMenu extends StatefulWidget {
 
 class _MainMenuState extends State<MainMenu> {
   final List<String> imageList = [
+    'assets/images/budidaya_baru.jpg',
+    'assets/images/panen_baru.jpg',
+    'assets/images/pascapanen_baru.jpg',
+    'assets/images/laporan_baru.jpg',
+    'assets/images/tokokopi.png',
     'assets/images/budidaya.png',
-    'assets/images/panen.png',
-    'assets/images/pascapanen.png',
-    'assets/images/laporan.png',
-    'assets/images/tokokopi.png', // Gambar Toko Kopi
-    'assets/images/resepkopi.png', // Gambar Resep Kopi
   ];
 
   final List<String> menuList = [
@@ -24,8 +24,8 @@ class _MainMenuState extends State<MainMenu> {
     'Panen',
     'Pasca_Panen',
     'Laporan',
-    'Toko_Kopi', // Menambahkan Toko Kopi
-    'Resep_Kopi', // Menambahkan Resep Kopi
+    'Toko_Kopi',
+    'Resep_Kopi',
   ];
 
   final List<String> labelMenu = [
@@ -33,84 +33,79 @@ class _MainMenuState extends State<MainMenu> {
     'Panen',
     'PascaPanen',
     'Laporan',
-    'Toko Kopi', // Menambahkan label Toko Kopi
-    'Resep Kopi', // Menambahkan label Resep Kopi
+    'Toko Kopi',
+    'Resep Kopi',
   ];
 
-  List<bool> isPressed = [
-    false,
-    false,
-    false,
-    false,
-    false, // Status untuk Toko Kopi
-    false, // Status untuk Resep Kopi
-  ];
+  List<bool> isPressed = List.generate(6, (_) => false);
 
   void _handleTap(int index) {
-  setState(() {
-    print('Menu yang dipilih: ${menuList[index]}');
-    
-    // Menambahkan logika untuk menavigasi ke halaman Toko Kopi dan Resep Kopi
-    if (menuList[index] == 'Toko_Kopi') {
-      print('Navigasi ke Toko Kopi');
-      Get.toNamed(RouteName.tokoKopi);  // Navigasi ke Toko Kopi
-    } else if (menuList[index] == 'Resep_Kopi') {
-      print('Navigasi ke Resep Kopi');
-      Get.toNamed(RouteName.resepKopi);  // Navigasi ke Resep Kopi
-    } else {
-      print('Navigasi ke kegiatan: ${RouteName.kegiatan}/${menuList[index]}');
-      Get.toNamed('${RouteName.kegiatan}/${menuList[index]}');  // Untuk menu lainnya
-    }
-  });
-}
+    setState(() {
+      print('Menu yang dipilih: ${menuList[index]}');
 
+      if (menuList[index] == 'Toko_Kopi') {
+        print('Navigasi ke Toko Kopi');
+        Get.toNamed(RouteName.tokoKopi);
+      } else if (menuList[index] == 'Resep_Kopi') {
+        print('Navigasi ke Resep Kopi');
+        Get.toNamed(RouteName.resepKopi);
+      } else {
+        print('Navigasi ke kegiatan: ${RouteName.kegiatan}/${menuList[index]}');
+        Get.toNamed('${RouteName.kegiatan}/${menuList[index]}');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      padding: EdgeInsets.all(15.0),
+      padding: const EdgeInsets.all(15.0),
       child: GridView.builder(
         shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
           crossAxisSpacing: 16,
           mainAxisSpacing: 16,
-          childAspectRatio: 1.5,
+          childAspectRatio: 100 / 110,
         ),
         itemCount: imageList.length,
         itemBuilder: (context, index) {
           return GestureDetector(
             onTap: () {
-              _handleTap(index); // Memanggil fungsi navigasi saat menu ditekan
+              _handleTap(index);
             },
             child: AnimatedContainer(
-              duration: Duration(milliseconds: 100),
+              duration: const Duration(milliseconds: 100),
               decoration: BoxDecoration(
-                color: isPressed[index] ? Colors.blue[900] : Color(0xffD4ECFF),
+                color: isPressed[index] ? Colors.blue[900] : Colors.transparent,
                 border: Border.all(
                   color: Colors.black.withOpacity(0.5),
                   width: 3.0,
                 ),
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    imageList[index],
-                    width: 50,
-                    height: 50,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    labelMenu[index], // Menampilkan nama menu sesuai list
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
+              child: SizedBox(
+                width: 100,
+                height: 110,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      imageList[index],
+                      width: 50,
+                      height: 50,
+                      fit: BoxFit.cover,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      labelMenu[index],
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
